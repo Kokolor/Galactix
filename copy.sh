ae91f742cd6a6bb495564a870b152c8b48f37e57
Kokolor <kokolor@acer-aspire-5.home>
2023-07-11 11:09:17 +0200

    Grub

--- copy.sh
+++ copy.sh
@@ -1,11 +1,32 @@
+#!/bin/bash
+
 gcc -static -o init source/init.c
 mkdir -p initramfs/
 cp -r linux/* initramfs/
 cp init initramfs/init
+
 cd initramfs
 find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../initramfs.img
 cd ..
-qemu-system-x86_64 -kernel linux/boot/bzImage -initrd initramfs.img
+
+mkdir -p cdrom/boot/grub
+
+cp linux/boot/bzImage cdrom/boot/
+cp initramfs.img cdrom/boot/
+
+echo 'set timeout=30
+set default=0
+
+menuentry "Galactix" {
+   linux /boot/bzImage ro quiet
+   initrd /boot/initramfs.img
+}' > cdrom/boot/grub/grub.cfg
+
+grub-mkrescue -o galactix.iso cdrom/
+qemu-system-x86_64 -cdrom galactix.iso
+
 rm initramfs.img
 rm -rf initramfs
 rm init
+rm -r cdrom/
+rm galactix.iso
